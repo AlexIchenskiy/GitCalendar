b1193133c10c8eb052ba88eadca94a4603b278a9
AlexIchenskiy <aichenskiy@gmail.com>
2023-02-03 14:31:31 +0100

    initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>Git Calendar</h1>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import Calendar from "../../components/Calendar";
+import Header from "../../components/Header";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Calendar />
+    </>
+  );
+};
+
+export default Home;
