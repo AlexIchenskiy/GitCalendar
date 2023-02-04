8b23e3d0bd8fc3e9f7b4057180e8729df71d3fdb
AlexIchenskiy <aichenskiy@gmail.com>
2023-02-04 09:36:17 +0100

    feat: add calendar table head

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,11 +1,17 @@
 import "./Calendar.scss";
 import "./Calendar.responsive.scss";
+import CalendarDays from "./components/CalendarDays/CalendarDays";
 
 const Calendar = () => {
   return (
     <div className="calendar">
       <div className="calendar-info"></div>
-      <div className="calendar-table"></div>
+      <div className="calendar-table">
+        <table>
+          <CalendarDays />
+          <tbody></tbody>
+        </table>
+      </div>
     </div>
   );
 };
---
+++ src/components/Calendar/components/CalendarDays/CalendarDays.js
@@ -0,0 +1,49 @@
+import { useEffect, useMemo, useState } from "react";
+
+import useWindowDimensions from "../../../../hooks/useWindowDimensions";
+
+const daysarr = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+function calculateDays(width) {
+  if (width < 992) {
+    let newDays = daysarr.map((el) => {
+      return el.slice(0, 3);
+    });
+    return newDays;
+  } else {
+    return daysarr;
+  }
+}
+
+const CalendarDays = () => {
+  const { width } = useWindowDimensions();
+
+  const [days, setDays] = useState(daysarr);
+  const memoDays = useMemo(() => calculateDays(width), [width]);
+
+  useEffect(() => {
+    setDays(memoDays);
+  }, [memoDays]);
+
+  return (
+    <>
+      <thead>
+        <tr>
+          {days.map((day) => (
+            <th key={day}>{day.toUpperCase()}</th>
+          ))}
+        </tr>
+      </thead>
+    </>
+  );
+};
+
+export default CalendarDays;
