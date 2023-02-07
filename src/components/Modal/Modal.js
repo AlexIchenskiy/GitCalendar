4ad91ef4ef38b043e45313a763e1849d7b23ea67
AlexIchenskiy <aichenskiy@gmail.com>
2023-02-07 12:50:39 +0100

    fix: fixed modal logic and styling

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,22 +1,31 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 
 import "./Modal.scss";
 
-const Modal = ({ title = "", children, isVisible = false }) => {
+const Modal = ({ title = "", children, isVisible, onClose }) => {
   const [visible, setVisible] = useState(isVisible);
+
+  const handleClose = () => {
+    setVisible(false);
+    onClose();
+  };
+
+  useEffect(() => {
+    setVisible(isVisible);
+  }, [isVisible]);
 
   const root = document.getElementById("root");
 
   return ReactDOM.createPortal(
     <div
       className={"modal-wrapper " + (visible ? "opened" : "closed")}
-      onClick={() => setVisible(false)}
+      onClick={() => handleClose()}
     >
       <div className="modal" onClick={(e) => e.stopPropagation()}>
         <div className="modal-info">
           <span>{title}</span>
-          <div className="modal-close-button" onClick={() => setVisible(false)}>
+          <div className="modal-close-button" onClick={() => handleClose()}>
             <span></span>
             <span></span>
           </div>
