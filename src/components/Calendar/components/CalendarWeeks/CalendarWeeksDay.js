564a28b773cf54e003380ce625fcd6bb26c8d3ee
AlexIchenskiy <aichenskiy@gmail.com>
2023-02-08 18:21:53 +0100

    chore: refactor responsive logic and styling

--- src/components/Calendar/components/CalendarWeeks/CalendarWeeksDay.js
+++ src/components/Calendar/components/CalendarWeeks/CalendarWeeksDay.js
@@ -7,54 +7,62 @@ const CalendarWeeksDay = ({ day, width, height }) => {
     <div className={'table-day ' + (day.active ? 'active' : 'inactive')}>
       <div className="table-day-info">
         <span>{day.value}</span>
-        {day.commits && day.commits.length > 1 && width > 768
-          ? (
+        {day.commits && day.commits.length > 1 && width > 768 && (
           <span className="table-day-info-more">
             +{day.commits.length - 1} more
           </span>
-            )
-          : null}
+        )}
       </div>
-      {day.commits && day.commits[0] && width > 587
-        ? (
-        <div className="table-day-description">
-          <span>
-            {moment(day.commits[0].commit.author.date).format('HH:mm')}
-          </span>
-          <span>
-            {day.commits[0].commit.message.length >
-            (width > 892 ? Math.floor(width / 120) : Math.floor(width / 150))
-              ? day.commits[0].commit.message
-                .slice(
-                  0,
-                  width > 1280
-                    ? Math.floor(width / 110)
-                    : width > 892
-                      ? Math.floor(width / 130)
-                      : Math.floor(width / 150)
-                )
-                .trim() + '...'
-              : day.commits[0].commit.message}
-          </span>
-        </div>
-          )
-        : (
-        <div className="table-day-lines">
-          {day.commits
-            ? day.commits.map((commit, index) =>
-              commit && index < Math.floor(height / 120)
-                ? (
-                  <span key={index} style={{ opacity: 1 - 0.2 * index }}></span>
-                  )
-                : null
+      {day.commits &&
+        day.commits[0] &&
+        (width > 587
+          ? (
+          <div className="table-day-description">
+            <span>
+              {moment(day.commits[0].commit.author.date).format('HH:mm')}
+            </span>
+            <span>
+              {sliceMessageByWidth(day.commits[0].commit.message, width)}
+            </span>
+          </div>
             )
-            : null}
-        </div>
-          )}
+          : (
+          <div className="table-day-lines">
+            {day.commits.map(
+              (commit, index) =>
+                index < Math.floor(height / 120) && (
+                  <span key={index} style={{ opacity: 1 - 0.2 * index }}></span>
+                )
+            )}
+          </div>
+            ))}
     </div>
   )
 }
 
-CalendarWeeksDay.propTypes = { day: PropTypes.object, width: PropTypes.number, height: PropTypes.number }
+CalendarWeeksDay.propTypes = {
+  day: PropTypes.object,
+  width: PropTypes.number,
+  height: PropTypes.number
+}
+
+const sliceMessageByWidth = (message, width) => {
+  // magic number to shorten the message
+  let cutBy = 160
+  if (width > 892) {
+    if (width > 1280) {
+      cutBy = 105
+    } else {
+      cutBy = 130
+    }
+  }
+
+  // if the message is already short enough, return it
+  if (message.length <= Math.floor(width / cutBy)) {
+    return message
+  }
+
+  return message.slice(0, Math.floor(width / cutBy)).trim() + '...'
+}
 
 export default CalendarWeeksDay
